faf902c93fb203b86f208e3ba6528fb4f709f170
Kamal <kamalfolarin@gmail.com>
2023-03-16 12:52:31 -0700

    divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,4 @@ def divisible_by_2(my_list=[]):
     l = len(my_list)
     boo = [True, False, True, False, True, False]
     for i in range(l):
-        if my_list[i] / 2 == 0:
-            return boo
-        else:
             return boo
