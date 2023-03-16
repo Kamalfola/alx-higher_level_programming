4ad55b8d8fd5b41b43b07ae323595a5fcaae6ede
Kamal <kamalfolarin@gmail.com>
2023-03-16 03:32:35 -0700

    tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    l = len(tuple_a)
+    m = len(tuple_b)
+    s = []
+    if l < 2:
+        if l == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if m < 2:
+        if m == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
